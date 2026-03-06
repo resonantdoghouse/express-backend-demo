9190ae2e0b87af98706af0466e30e5b49a6e9887
Jim <jim@advancebass.com>
2026-03-05 16:59:47 -0800

    fix sql path

--- src/utils/import-database.js
+++ src/utils/import-database.js
@@ -2,7 +2,7 @@ import connection from "./mysql.js";
 import fs from "fs";
 
 try {
-  const sql = fs.readFileSync("../sql/database.sql").toString();
+  const sql = fs.readFileSync("sql/database.sql").toString();
   await connection.query(sql);
   console.log("Database imported");
 } catch (error) {
